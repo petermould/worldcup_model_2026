4ce3973b7ac08f9ac3acefea43465a6af773ba10
petermould <petermould2002@gmail.com>
2026-07-01 15:36:08 +0100

    Add files via upload

---
+++ tournament_state.py
@@ -0,0 +1,24 @@
+# Update this file after each match
+# Last updated: July 1 2026
+
+CONFIRMED_THROUGH = [
+    'Canada',
+    'Brazil',
+    'Paraguay',
+    'Morocco',
+    'France',
+    'Norway',
+    'Mexico',
+]
+
+REMAINING_MATCHES = [
+    ('England', 'Congo DR'),
+    ('Belgium', 'Senegal'),
+    ('USA', 'Bosnia'),
+    ('Spain', 'Austria'),
+    ('Portugal', 'Croatia'),
+    ('Switzerland', 'Algeria'),
+    ('Australia', 'Egypt'),
+    ('Argentina', 'Cabo Verde'),
+    ('Colombia', 'Ghana'),
+]
