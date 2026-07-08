2b9ceaf378a1db86ee032e362c4cae37dcc6e7a3
petermould <petermould2002@gmail.com>
2026-07-08 16:54:59 +0100

    Update tournament_state.py

--- tournament_state.py
+++ tournament_state.py
@@ -1,23 +1,17 @@
-# Last updated: July 2 2026
+# Last updated: July 8 2026
+ 
+# Teams already confirmed WINNERS of a Quarterfinal (none yet - QFs start July 9)
+CONFIRMED_THROUGH = []
+ 
+# Quarterfinal matchups - none played yet as of July 8 2026
 
-CONFIRMED_THROUGH = [
-    'Canada',
-    'Brazil',
-    'Paraguay',
-    'Morocco',
-    'France',
-    'Norway',
-    'Mexico',
-    'England',
-    'Belgium',
-    'USA',
-    'Portugal',
-    'Switzerland',
-    'Spain',
-]
 
 REMAINING_MATCHES = [
-    ('Australia', 'Egypt'),
-    ('Argentina', 'Cabo Verde'),
-    ('Colombia', 'Ghana'),
+    ('France', 'Morocco'),         # Thu Jul 9, Boston
+    ('Spain', 'Belgium'),          # Fri Jul 10, Los Angeles
+    ('Norway', 'England'),         # Sat Jul 11, Miami
+    ('Argentina', 'Switzerland'),  # Sat Jul 11, Kansas City
 ]
+ 
+
+ 
